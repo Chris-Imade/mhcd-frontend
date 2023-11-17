1bc5d45e154e77803cb23b845f12eef59b882ce7
Chris Imade <jephthahimade@gmail.com>
2023-11-17 13:04:03 +0100

    added some images

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Quote from "../../images/quote.png";
 import styles from "./Testimonials.module.css";
+import Test1 from "../../images/test-1.jpg";
+import Test2 from "../../images/test-2.jpg";
+import Test3 from "../../images/test-3.jpg";
 
 const Testimonials = () => {
     const [index, setIndex] = useState(0);
@@ -10,16 +13,19 @@ const Testimonials = () => {
             message: 'I can boldly say that I for one have seen and experience the value in MHCD ACademy’s course. I took the web train and have been enjoying it ever since.',
             name: 'Chinanza Adaeze',
             profession: 'Student',
+            image: Test1
         },
         {
             message: 'MHCD has changed my life and the way I look at the internet. I took a course in Cyber Security and ever since I have been exploiting vulnerabilities for the sole purpose of being the fix.',
             name: 'Anema David',
             profession: 'Student',
+            image: Test2
         },
         {
             message: 'I have embraced the concept of working from home since the lock-down and thus learning from the comfort of my home is definitely invaluable to me.',
             name: 'King Solomon',
             profession: 'Student',
+            image: Test3
         },
     ];
 
@@ -31,7 +37,7 @@ const Testimonials = () => {
         
         <img src={Quote} alt="Quote" width={71} height={71} className='my-[16px] ' />
         <p className='lg:w-[655px] text-[22px] font-regular text-center'>{testimonies[index].message}</p>
-        <div className='w-[100px] h-[100px] bg-slate-400 rounded-full mt-[21px] mb-[10px]'></div>
+        <img className='w-[100px] h-[100px] rounded-full mt-[21px] mb-[10px] object-contain' src={testimonies[index].image} alt="" />
         <h3 className='text-[22px] font-medium'>{testimonies[index].name}</h3>
         <p className='text-[10.62px] font-normal text-[#6A6A6A]'>{testimonies[index].name}</p>
             
