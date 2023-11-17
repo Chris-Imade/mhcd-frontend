d914033e0b677e100dd202ff7c469dab8553dcfa
Chris Imade <jephthahimade@gmail.com>
2023-11-17 07:33:07 +0100

    updated the package

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Loader from "./components/Loader";
 import NavigationContainer from "./components/Nav/NavigationContainer";
-import Hero from "./components/Hero";
-import AboutUs from "./components/About/AboutUs";
-import Subjects from "./components/Subjects/Subjects";
-import Course from "./Courses/Course";
-import SignUp from "./components/SignUp/SignUp";
-import Partners from "./components/Partners/Partners";
-import Testimonials from "./components/Testimonials/Testimonials";
-import Footer from "./Footer/Footer";
-import Register from "./Screens/Auth/Register";
-import Login from "./Screens/Auth/Login";
 import Dashboard from "./Screens/Dashboard/Dashboard";
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import Root from "./components/Root";
+import Login from "./Screens/Auth/Login";
+import Register from "./Screens/Auth/Register";
 
 
 function App() {
@@ -28,14 +20,18 @@ function App() {
   }, [])
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<Root />} />
-        <Route path="/login/*" element={<Login />} />
-        <Route path="/register/*" element={<Register />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <>
+      {showLoader ? <Loader /> : (
+      <Router>
+        <Routes>
+          <Route path="/*" element={<Root />} />
+          <Route path="/login/*" element={<Login />} />
+          <Route path="/register/*" element={<Register />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+        </Routes>
+      </Router>
+      )}
+    </>
   );
 }
 
